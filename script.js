297097e43afd3113752bc47a5c38923f26703d6c
Aaron Strouse <aaronstrouse@gmail.com>
2019-01-11 16:32:37 -0500

    animation retained and removed zero problem

--- script.js
+++ script.js
@@ -2,26 +2,27 @@ const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
+//  Using the Date object to grab the real time and getHours/Minutes/Seconds methods
+var date = new Date();
+let hr = date.getHours();
+let min = date.getMinutes();
+let sec = date.getSeconds();
+
+//  These positions are determined by divding the 360 degrees of the circle by 60(seconds or minutes) or 12 (hours)
+let hrPosition = (hr*360/12)+(min*(360/60)/12);
+let minPosition = (min*360/60)+(sec*(360/60)/60); //take current minute add number of seconds to clock then divide into 60 increments
+let secPosition = sec*360/60;
 
 function runTheClock() { 
-    //  Using the Date object to grab the real time and getHours/Minutes/Seconds methods
-    var date = new Date();
-    let hr = date.getHours();
-    let min = date.getMinutes();
-    let sec = date.getSeconds();
 
-    //  These positions are determined by divding the 360 degrees of the circle by 60(seconds or minutes) or 12 (hours)
-    let hrPosition = (hr*360/12)+(min*(360/60)/12);
-    let minPosition = (min*360/60)+(sec*(360/60)/60); //take current minute add number of seconds to clock then divide into 60 increments
-    let secPosition = sec*360/60;
-
-
-    // Apply these numbers as degrees as inline CSS using transform property
+    hrPosition = hrPosition+(3/360);
+    minPosition = minPosition+(6/60);
+    secPosition = secPosition+6;
+    
     HOURHAND.style.transform = "rotate(" + hrPosition +  "deg)";
     MINUTEHAND.style.transform = "rotate(" + minPosition +  "deg)";
     SECONDHAND.style.transform = "rotate(" + secPosition +  "deg)";
 }
 
-//  setInterval repeatedly calls a function with the default set to milliseconds.
-// Every 1 second interval will call the function runTheClock
+
 var interval = setInterval(runTheClock, 1000)
