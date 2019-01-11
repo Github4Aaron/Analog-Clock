ed9d609fd668a561b6acadd61585889278f31ef8
Aaron Strouse <aaronstrouse@gmail.com>
2019-01-11 15:09:44 -0500

    added Date object combined with getHours/getMinutes/getSeconds methods

--- script.js
+++ script.js
@@ -7,8 +7,16 @@ let hrPosition = 20;
 let minPosition = 130;
 let secPosition = 147;
 
-// Apply these numbers as degrees as inline CSS using transform property
+//  Using the Date object to grab the real time and getHours/Minutes/Seconds methods
+var date = new Date();
+let hr = date.getHours();
+let min = date.getMinutes();
+let sec = date.getSeconds();
 
+
+
+// Apply these numbers as degrees as inline CSS using transform property
 HOURHAND.style.transform = "rotate(" + hrPosition +  "deg)";
 MINUTEHAND.style.transform = "rotate(" + minPosition +  "deg)";
 SECONDHAND.style.transform = "rotate(" + secPosition +  "deg)";
+
